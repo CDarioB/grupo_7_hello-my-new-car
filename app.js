67c04f399db64f3c01ff583963f4c2a1875a3dd4
stivennevits <stivennevits16@gmail.com>
2022-10-17 13:36:35 -0500

    detalle Del Producto

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 
-const EXPRESS = require ('express'); 
-const PATH = require ('path'); 
-
+const EXPRESS = require ("express"); 
+const PATH = require ("path"); 
 const APP = EXPRESS();
-const PUBLIC_PATH = PATH.resolve(__dirname,'./public');
-
+const PUBLIC_PATH = PATH.join(__dirname, "./public");
 const PORT = process.env.PORT || 3000;
-
 APP.use(EXPRESS.static(PUBLIC_PATH));
-
-
-APP.get('/', (req, res) => {
-    res.sendFile(PATH.resolve(__dirname, './views/home.html'))
-});
-
-APP.get("/register", (req, res) => {
-    res.sendFile(PATH.resolve(__dirname, "./views/register.html"))
-});
-
-APP.get("/login", (req, res) => {
-    res.sendFile(PATH.resolve(__dirname, "./views/login.html"))
-});
-
 
 APP.listen(PORT, () =>{console.log("Corriendo servidor en el puerto" + " " + PORT + " " + "http://localhost:" + PORT) } )
 
-// app.listen (PORT, () => console.log (`Server started: port ${PORT}`) ); 
+APP.get("/", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/home.html"))
+});
+
+APP.get("/register", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/register.html"))
+});
+
+APP.get("/login", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/login.html"))
+});
+
+APP.get("/detalleDelProducto", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./detalleDelProducto.html"))
+});
+
