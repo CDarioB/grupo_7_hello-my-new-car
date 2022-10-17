b752702c316f05093af696f7a76eb26d3ea7aa57
stivennevits <stivennevits16@gmail.com>
2022-10-17 18:25:14 -0500

    detalle de producto realizado

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ APP.get("/login", (req, res) => {
 });
 
 APP.get("/detalleDelProducto", (req, res) => {
-    res.sendFile(PATH.join(__dirname , "./detalleDelProducto.html"))
+    res.sendFile(PATH.join(__dirname , "./views/detalleDelProducto.html"))
 });
 
+APP.get("/detalle2", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalle2.html"))
+});
+
+APP.get("/detalle3", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalle3.html"))
+});
+
+APP.get("/detalle4", (req, res) => {
+    res.sendFile(PATH.join(__dirname , "./views/detalle4.html"))
+});
