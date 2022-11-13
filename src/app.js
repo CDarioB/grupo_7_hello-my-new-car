2737e80f712b1124a46f65fb4159445a3178ded7
CDarioB <carlosdario1@gmail.com>
2022-11-13 16:13:08 -0300

    Refactor app.js -> src/app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 const express = require ("express"); 
 const path = require ("path"); 
 const app = express();
-const PUBLIC_PATH = path.join(__dirname, "./public");
+const PUBLIC_PATH = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3000;
 app.use(express.static(PUBLIC_PATH));
 
 // Levantando el servidor
-app.listen(PORT, () =>{console.log("Corriendo servidor en el puerto" + " " + PORT + " " + "http://localhost:" + PORT) } )
+app.listen(PORT, () =>{console.log("Corriendo servidor en el puerto:" + " " + PORT + " - url: " + "http://localhost:" + PORT) } )
 
 // EJS
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
 // Creando rutas
-var indexRouter = require('./src/routes/index');
-var usuariosRouter = require('./src/routes/usuarios');
-var productosRouter = require('./src/routes/productos');
+var indexRouter = require('./routes/index');
+var usuariosRouter = require('./routes/usuarios');
+var productosRouter = require('./routes/productos');
 
 // Manejo de vistas con rutas
 app.use('/', indexRouter);
