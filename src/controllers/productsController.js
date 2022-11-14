d74c06d767881bfae465586f6e1f0cdccc4fe918
CDarioB <carlosdario1@gmail.com>
2022-11-13 23:32:15 -0300

    data base example json - products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -33,7 +33,7 @@ const productsController = {
             "prd_model_year": req.body.modelYearCar,
             "prd_mileage": req.body.mileageCar,
             "prd_price": req.body.priceCar,
-            "prd_discount": req.body.discountCar,
+            "prd_discount_Percentage": req.body.priceCar - ((req.body.priceCar * req.body.discountCar)/100),
             "prd_img": imgList
         };
         products.push(newProducts);
