8e0726149c8030351b1f9c2c85880564e9894b46
CDarioB <carlosdario1@gmail.com>
2023-01-08 19:39:58 -0300

    se corrigen algunas vistas, y se edita los metodos del productController. Revisar Issuses con notas de lo faltante.

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -89,64 +89,79 @@ const productsController = {
         }
     },
     modify: (req,res,next) => {
-        res.render('partials/product/productListModify', {
-                products
-            });
+        Products.findAll({
+            include: ['province']
+            //include: ['province','category']
+        }).then(products => {
+           for(let i = 0; i < products.length; i++){
+            products[i].img = products[i].img.split(",");
+           }
+           res.render('partials/product/productListModify',{products});
+        });
     },
 
     edit: (req, res, next) => {
-        const id = req.params.id;
-        const product = products.find(product => product.prd_id == id);
-        res.render('partials/product/editProduct', {product});
+        //const id = req.params.id;
+        //const product = products.find(product => product.prd_id == id);
+        let allCategory;
+        let productPkId;
+        let allProvinces;
+        
+        Products.findByPk(
+                req.params.id,
+                {include: ['province']} //{include: ['province','category']}
+        ).then(prodId => { 
+            productPkId = prodId;
+            productPkId.img = productPkId.img.split(",");
+        })
+        
+        Provinces.findAll()
+            .then(provinces => {
+                allProvinces = provinces
+        })
+
+        Categories.findAll()
+            .then(categories => {
+                allCategory = categories;
+            }).then(() => {
+                res.render('partials/product/editProduct',{product: productPkId, allProvinces: allProvinces, allCategory: allCategory});
+        });
     },
     update: (req, res) => {
-        const id =req.params.id;
-        const productToEdit = products.find(product => product.prd_id == id);
-
         let discount = req.body.discountCar ? parseInt(req.body.discountCar) : 0;
         let price = parseInt(req.body.priceCar);
-
-        /*let img = {};
-        let id_img = productToEdit.prd_img.length;
-        if (req.files) {
-            for (let i=0; i < req.files.length; i++) {
-                img = {
-                    "img_id": id_img + 1,
-                    "img_fileName": "/img/"+req.files[i].filename
-                }; 
-    
-                productToEdit.prd_img.push(img);
-            }
-        }*/
         
-        let editProduct = {
-            "prd_id": id,
-            "prd_references": req.body.refCar,
-            "prd_brand": req.body.brandCar,
-            "prd_province": req.body.provinceCar,
-            //"prd_city": req.body.cityCar,
-            "prd_category_type": req.body.categoryTypeCar,
-            "prd_model_year": req.body.modelYearCar,
-            "prd_mileage": req.body.mileageCar,
-            "prd_price": price,
-            "prd_discount_Percentage": discount,
-            "prd_discountprice": price - ((price * discount)/100),
-            "prd_img": productToEdit.prd_img
-        }
-
-
-        products.forEach((product, index) => {
-            if (product.prd_id == id) {
-                products [index] = editProduct;
+        Products.update({
+            //created_at: moment(dateNow).locale('es-us').format('YYYY-MM-DD'),
+            //updated_at: moment(dateNow).locale('es-us').format('YYYY-MM-DD'),
+            references: req.body.refCar,
+            brand: req.body.brandCar,
+            model: req.body.modelYearCar,
+            mileage: req.body.mileageCar,
+            price: price,
+            discount_percentage: discount,
+            discount_price: price - ((price * discount)/100),
+            //img: images.join(','),
+            category_id: req.body.categoryTypeCar,
+            province_id: req.body.provinceCar,
+            // location_id: req.body.cityCar,
+            user_id: 1
+        },{
+            where: {
+                id: req.params.id
             }
-        });
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
-        
-        res.render('partials/product/productListModify', {
-            products
-        });
+        }).then(()=> {
+            Products.findAll({
+                include: ['province']
+                //include: ['province','category']
+            }).then(products => {
+               for(let i = 0; i < products.length; i++){
+                products[i].img = products[i].img.split(",");
+               }
+               res.render('partials/product/productListModify',{products});
+            });
+        })
     },
-
     menuModificar: function(req,res,next) {
         res.render('./partials/product/modificarMenu');
     },
