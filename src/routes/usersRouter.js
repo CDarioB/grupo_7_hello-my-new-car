81260d5b4669820f00b7927a7d781f3678632ebc
AndresFSerrano <104331097+AndresFSerrano@users.noreply.github.com>
2023-01-12 00:27:09 -0500

    Revert "Login"

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,24 +2,9 @@ const express = require ("express");
 const router = express.Router();
 
 const usersController = require('../controllers/usersController.js');
+const uploadFile = require('../middlewares/multerMiddleware');
 
 //Middlewares
-const uploadFile = require('../middlewares/multerMiddleware');
-const validateLogin = require('../middlewares/validateLoginMiddleware')
-
-//Login
-router.get('/login', usersController.login);
-router.post('/login', validateLogin, usersController.processLogin)
-router.get('/check', function(req, res){
-    if (req.session.usuarioLogeado == undefined) {
-        res.send('El usuario no está logeado')
-    } else {
-        res.send('El usuario logeado es ' + req.session.usuarioLogeado)
-    }
-})
-
-//Recuperar cuenta de usuario
-router.get('/recovery', usersController.recovery);
 
 // Consulta de users
 router.get('/', usersController.users);
