8e060c71796d5ac1fec5f3932b0902779b5a1c44
CDarioB <carlosdario1@gmail.com>
2023-02-07 14:37:44 -0300

    info doc - sprint 8 pdf

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use('/products', productsRouter);
 
 
 // Levantando el servidor
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
